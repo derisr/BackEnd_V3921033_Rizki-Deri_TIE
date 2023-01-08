010b90bc3c6c60e4f9896281356763d56fc63381
derisr <111950188+derisr@users.noreply.github.com>
2023-01-08 19:37:01 +0700

    Add files via upload

---
+++ database/migrations/2023_01_07_052447_create_fasilitas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fasilitas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_fasilitas');
+            $table->string('id_fasilitas');
+            $table->string('jumlah_produk');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fasilitas');
+    }
+};
